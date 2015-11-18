af04004bfad4049037d0b88bbfeac2f2ed66e179
funimu <hpmp4949@gmail.com>
2015-11-18 14:28:44 +0900

    add controller

--- src/jp/ac/board/controller/BoardContoroller.java
+++ src/jp/ac/board/controller/BoardContoroller.java
@@ -1,11 +1,15 @@
 package jp.ac.board.controller;
 
 import java.io.IOException;
+import java.util.HashMap;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import jp.ac.board.*;
 
 /**
  * Servlet implementation class BoardContoroller
@@ -17,9 +21,18 @@ public class BoardContoroller extends HttpServlet {
     /**
      * @see HttpServlet#HttpServlet()
      */
+	
+//	private HashMap<String,Model> map;
+	
     public BoardContoroller() {
         super();
         // TODO Auto-generated constructor stub
+//        map = new HashMap<String, Model>();
+//        //keyÇuriÇ…ÇµÇƒêÿÇËï™ÇØÇÈÇÒÇ‚Ç≈(ÉjÉbÉRÉä)
+//        map.put("/FrontControl/control/add", new Add());
+//        map.put("/FrontControl/control/sub", new Sub());
+//        map.put("/FrontControl/control/mult", new Mult());
+//        map.put("/FrontControl/control/div", new Div());
     }
 
 	/**
@@ -34,6 +47,17 @@ public class BoardContoroller extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		request.setCharacterEncoding("utf-8");
+		String methodType = request.getParameter("type");
+		//
+		if(methodType.equals("login")){
+			
+		} else if(methodType.equals("logout")){
+			
+		}else if(methodType.equals("register")){
+			
+		}else if(methodType.equals("posting")){
+			
+		}
 	}
-
 }
