124512e2365162310ac536a89f3ead4d2551fe9c
uedashota <uedashota.1329@gmail.com>
2015-11-24 22:12:36 +0900

    LoginModel

--- src/jp/ac/board/model/LoginModel.java
+++ src/jp/ac/board/model/LoginModel.java
@@ -1,5 +1,29 @@
 package jp.ac.board.model;
 
-public class LoginModel {
+import java.io.IOException;
+import java.io.PrintWriter;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginModel {
+	protected void doPost(HttpServletRequest request,HttpServletResponse response) throws ServletException,IOException {
+		//リクエストの文字コード設定
+		request.setCharacterEncoding("UTF-8");
+		
+		//リクエストから入力文字取得
+		String getString = request.getParameter("input");
+		
+		//入力の確認
+		if(getString == null || getString.isEmpty()){
+			getString = "入力されていません";
+		}
+		
+		//レスポンスのコンテンツおよび文字コード設定
+		response.setContentType("text/html;charset=UTF-8");
+		
+		//レスポンス出力オブジェクト作成
+		PrintWriter out = response.getWriter();
+	}
 }
