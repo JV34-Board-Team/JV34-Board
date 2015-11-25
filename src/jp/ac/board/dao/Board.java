2d2752bfe174465d947f69844a5199b453d603ed
Naventus <u.u.tau216@gmail.com>
2015-11-25 12:01:07 +0900

    Update Model

--- src/jp/ac/board/dao/Board.java
+++ src/jp/ac/board/dao/Board.java
@@ -6,57 +6,34 @@ import java.util.ArrayList;
 
 public class Board extends Dao {
 	
-	private String id;
+	private int id;
+	private int userId;
 	private String userName;
 	private String comment;
 	private String createdAt;
-	private ResultSet result;
 	
-	
-	public Board(String id, String userName, String comment, String createdAt) {
+	public Board(int id, int userId, String userName, String comment, String createdAt) {
 		super();
 		this.id = id;
+		this.userId = userId;
 		this.userName = userName;
 		this.comment = comment;
 		this.createdAt = createdAt;
 	}
 	
-	public String getId() {
-		return id;
-	}
-	public void setId(String id) {
-		this.id = id;
-	}
-	public String getUserName() {
-		return userName;
-	}
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
-	public String getComment() {
-		return comment;
-	}
-	public void setComment(String comment) {
-		this.comment = comment;
-	}
-	public String getCreatedAt() {
-		return createdAt;
-	}
-	public void setCreatedAt(String createdAt) {
-		this.createdAt = createdAt;
-	}
 	
 	public ArrayList<Board> getAll() {
 		ArrayList<Board> boards = new ArrayList<>();
 		open();
 		try {
-			result = selectAll("SELECT * FROM v_board;");
+			ResultSet result = selectAll("SELECT * FROM v_board;");
 			while(result.next()) {
 				boards.add(new Board(
-							result.getString("id"),
-							result.getString("userName"),
+							result.getInt("id"),
+							result.getInt("userid"),
+							result.getString("username"),
 							result.getString("comment"),
-							result.getString("createdAt")
+							result.getString("createdat")
 						));
 			}
 		} catch(SQLException e) {
@@ -71,13 +48,14 @@ public class Board extends Dao {
 		ArrayList<Board> boards = new ArrayList<>();
 		open();
 		try {
-			result = selectWhere("SELECT * FROM v_board where userName=?;", where);
+			ResultSet result = selectWhere("SELECT * FROM v_board where userName=?;", where);
 			while(result.next()) {
 				boards.add(new Board(
-							result.getString("id"),
-							result.getString("userName"),
+							result.getInt("id"),
+							result.getInt("userid"),
+							result.getString("username"),
 							result.getString("comment"),
-							result.getString("createdAt")
+							result.getString("createdat")
 						));
 			}
 		} catch(SQLException e) {
